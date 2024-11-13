af9b85754c7a6f32a3912e28993f3faf7fab68b7
Qusai80 <i220935@nu.edu.pk>
2024-11-13 12:09:20 +0500

    first commit

---
+++ src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create a Label with text
+        Label label = new Label("Hello, JavaFX!");
+
+        // Set up the root layout
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        // Create a Scene with specified width and height
+        Scene scene = new Scene(root, 300, 200);
+
+        // Set up the Stage (window)
+        primaryStage.setTitle("JavaFX Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
